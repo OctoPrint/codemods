211053e00ea2bde8ef7451939a57db573cf92389
Gina Häußge <gina@octoprint.org>
2020-10-07 16:03:43 +0200

    Better error handling part 2

--- octoprint_codemods/util.py
+++ octoprint_codemods/util.py
@@ -26,9 +26,7 @@ def transform_file(
         with open(filename, "r") as python_file:
             python_source = python_file.read()
     except Exception as exc:
-        raise TransformError(
-            "Could not read file {}, skipping: {}".format(filename, str(exc))
-        )
+        print("Could not read file {}, skipping: {}".format(filename, str(exc)))
 
     try:
         source_tree = cst.parse_module(python_source)
